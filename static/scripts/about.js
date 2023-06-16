ccc115d6551bbfc4674960f1f817a83fee9303c6
FebryanS <49833582+FebryanShino@users.noreply.github.com>
2023-06-16 08:01:19 +0700

    Add files via upload

--- static/scripts/about.js
+++ static/scripts/about.js
@@ -19,11 +19,20 @@ burgerContainer.addEventListener('click', (event) => {
 
 
 const loadAboutPage = (items) => {
+  let ownerGameID = document.querySelector('.profile > h6');
+  let bio = document.querySelector('.bio > h6');
+
+  ownerGameID.textContent = items[0];
+  bio.textContent = items[1];
+
+
+  
   let gridContainer = document.querySelector('.social');
   
-  for(let i = 0; i < items.length; i++) {
+  for(let i = 0; i < items[2].length; i++) {
     let item = document.createElement('a');
-    item.textContent = items[i];
+    item.textContent = items[2][i][0];
+    item.href = items[2][i][1];
     
     item.style.animation = 'fadeIn 400ms ease forwards';
     item.style.animationDelay = i*180 + 'ms';
